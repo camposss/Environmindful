2caeb162bf3eda16879ea6a107674910ea4d42a6
brucean52 <bruce.an52@gmail.com>
2018-02-15 13:56:01 -0800

    removed screen lock orientation

--- script.js
+++ script.js
@@ -16,8 +16,9 @@ callApi function to coordinate api calls.
  */
 function initializeApp() {
     var submit_button = $('#submit_button');
-    screen.orientation.lock('portrait').catch(function(){
-    });
+//    screen.orientation.lock('portrait').catch(function(){
+//    });
+    //window.screen.lockOrientation('portrait');
     submit_button.on('click', geocode);
     google.charts.load('current', { 'packages': ['corechart'] });
     callApi();
